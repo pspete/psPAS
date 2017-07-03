7af835628c564c9667119b3b8b35efe2e37ebd50
Pete Maan <pete.maan+github@gmail.com>
2017-07-03 14:25:38 +0100

    Add Get-SafeMembers Function
    List Safe members functionality from v 9.9

---
+++ Functions/SafeMembers/Get-PASSafeMembers.ps1
@@ -0,0 +1,84 @@
+﻿function Get-PASSafeMembers{
+<#
+.SYNOPSIS
+Lists the members of a Safe
+
+.DESCRIPTION
+Lists the members of a Safe.
+View Safe Members permission is required.
+
+.PARAMETER SafeName
+The name of the safe to get the members of
+
+.PARAMETER sessionToken
+Hashtable containing the session token returned from New-PASSession
+
+.PARAMETER WebSession
+WebRequestSession object returned from New-PASSession
+
+.PARAMETER BaseURI
+PVWA Web Address
+Do not include "/PasswordVault/"
+
+.EXAMPLE
+
+.INPUTS
+Session Token, SafeName, WebSession & BaseURI can be piped by property name
+
+.OUTPUTS
+TODO
+
+.NOTES
+
+.LINK
+#>
+    [CmdletBinding()]  
+    param(
+        [parameter(
+            Mandatory=$true,
+            ValueFromPipelinebyPropertyName=$true
+        )]
+        [ValidateNotNullOrEmpty()]
+        [string]$SafeName,
+
+        [parameter(
+            Mandatory=$true,
+            ValueFromPipelinebyPropertyName=$true
+        )]
+        [ValidateNotNullOrEmpty()]
+        [hashtable]$sessionToken,
+
+        [parameter(
+            ValueFromPipelinebyPropertyName=$true
+        )]
+        [Microsoft.PowerShell.Commands.WebRequestSession]$WebSession,
+
+        [parameter(
+            Mandatory=$true,
+            ValueFromPipelinebyPropertyName=$true
+        )]
+        [string]$BaseURI
+    )
+
+    BEGIN{}#begin
+
+    PROCESS{
+
+        #Create URL for request
+        $URI = "$baseURI/PasswordVault/WebServices/PIMServices.svc/Safes/$($SafeName | 
+        
+            Get-EscapedString)/Members"
+        
+        #Send request to webservice
+        $result = Invoke-PASRestMethod -Uri $URI -Method GET -Headers $sessionToken -WebSession $WebSession
+
+    }#process
+
+    END{
+    
+        #output
+        $result
+        
+    }#end
+
+}
