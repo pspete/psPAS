579fce8bef0af04204d180aeedc3682467c011c1
Pete Maan <pete.maan+github@gmail.com>
2017-07-03 17:22:31 +0100

    Add certificateserialnumber authentication method
    v9.9 states certificateserialnumber can be added as authetication method
    to applications.

--- Functions/Applications/Add-PASApplicationAuthenticationMethod.ps1
+++ Functions/Applications/Add-PASApplicationAuthenticationMethod.ps1
@@ -62,7 +62,7 @@ TODO: ParameterSets/DynamicParameters
         [parameter(
             Mandatory=$true
         )]
-        [ValidateSet("path","hash","osUser","machineAddress")]
+        [ValidateSet("path","hash","osUser","machineAddress","certificateserialnumber")]
         [string]$AuthType,
 
         [parameter(
@@ -103,9 +103,9 @@ TODO: ParameterSets/DynamicParameters
 
         }
 
-        if($AuthType -eq "hash"){
+        if(($AuthType -eq "hash") -or ($AuthType -eq "certificateserialnumber")){
             
-            #parameters only relevent to hash authentication
+            #add comment parmater
             New-DynamicParam -Name Comment -DPDictionary $Dictionary
 
         }
