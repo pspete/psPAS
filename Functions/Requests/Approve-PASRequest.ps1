207297e7b82acbd5f782c3d0bc99bd2515ed05b0
Pete Maan <pete.maan+github@gmail.com>
2017-12-16 23:13:09 +0000

    UPDATE Help
    Corrected output section of help text to reflect that no output is
    returned.

--- Functions/Requests/Approve-PASRequest.ps1
+++ Functions/Requests/Approve-PASRequest.ps1
@@ -35,11 +35,7 @@ Confirms request <ID>
 All parameters can be piped by property name
 
 .OUTPUTS
-SessionToken, WebSession, BaseURI are passed through and
-contained in output object for inclusion in subsequent
-pipeline operations.
-Output format is defined via psPAS.Format.ps1xml.
-To force all output to be shown, pipe to Select-Object *
+None
 
 .NOTES
 Minimum CyberArk Version 9.10
