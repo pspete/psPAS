c5cfec98573ebaf232880dec9f11f7971f745fe4
Pete Maan <pete.maan+github@gmail.com>
2018-01-17 12:54:56 +0000

    ADD Stop-PASPSMSession
    New function added: Stop-PASPSMSession
    Enables a user to invoke termination of a Live PSM Session. CyberArk
    10.1 required.
    
    Relates to #49

---
+++ Functions/Monitoring/Stop-PASPSMSession.ps1
@@ -0,0 +1,92 @@
+function Stop-PASPSMSession {
+	<#
+.SYNOPSIS
+Terminates a Live PSM Session.
+
+.DESCRIPTION
+Terminates a Live PSM Session identified by the unique ID of the PSM Session.
+
+.PARAMETER LiveSessionId
+The unique ID/SessionGuid of a Live PSM Session.
+
+.PARAMETER sessionToken
+Hashtable containing the session token returned from New-PASSession
+
+.PARAMETER WebSession
+WebRequestSession object returned from New-PASSession
+
+.PARAMETER BaseURI
+PVWA Web Address
+Do not include "/PasswordVault/"
+
+.PARAMETER PVWAAppName
+The name of the CyberArk PVWA Virtual Directory.
+Defaults to PasswordVault
+
+.EXAMPLE
+$token | Stop-PASPSMSession -LiveSessionId <Session UUID>
+
+Terminates Live PSM Session identified by the session UUID.
+
+.INPUTS
+All parameters can be piped by property name
+
+.OUTPUTS
+None
+
+.NOTES
+Minimum CyberArk Version 10.1
+
+.LINK
+
+#>
+	[CmdletBinding()]
+	param(
+		[parameter(
+			Mandatory = $true,
+			ValueFromPipelinebyPropertyName = $true
+		)]
+		[ValidateNotNullOrEmpty()]
+		[Alias("SessionGuid")]
+		[string]$LiveSessionId,
+
+		[parameter(
+			Mandatory = $true,
+			ValueFromPipelinebyPropertyName = $true
+		)]
+		[ValidateNotNullOrEmpty()]
+		[hashtable]$sessionToken,
+
+		[parameter(
+			ValueFromPipelinebyPropertyName = $true
+		)]
+		[Microsoft.PowerShell.Commands.WebRequestSession]$WebSession,
+
+		[parameter(
+			Mandatory = $true,
+			ValueFromPipelinebyPropertyName = $true
+		)]
+		[string]$BaseURI,
+
+		[parameter(
+			Mandatory = $false,
+			ValueFromPipelinebyPropertyName = $true
+		)]
+		[string]$PVWAAppName = "PasswordVault"
+	)
+
+	BEGIN {}#begin
+
+	PROCESS {
+
+		#Create URL for Request
+		$URI = "$baseURI/$PVWAAppName/api/LiveSessions/$($LiveSessionId | Get-EscapedString)/Terminate"
+
+		#send request to PAS web service
+		Invoke-PASRestMethod -Uri $URI -Method POST -Headers $sessionToken -WebSession $WebSession
+
+	} #process
+
+	END {}#end
+
+}
