4517b0eda16083b46cf929d5df1c8ab1f3fbeeb1
Pete Maan <pete.maan+github@gmail.com>
2018-02-11 17:40:17 +0000

    ADD build.ps1
    Initial commit of build.ps1.

---
+++ build/build.ps1
@@ -0,0 +1,13 @@
+#---------------------------------#
+# Header                          #
+#---------------------------------#
+Write-Host 'Running build script' -ForegroundColor Yellow
+Write-Host "ModuleName    : $env:APPVEYOR_PROJECT_NAME"
+Write-Host "Build version : $env:APPVEYOR_BUILD_VERSION"
+Write-Host "Author        : $env:APPVEYOR_REPO_COMMIT_AUTHOR"
+Write-Host "Branch        : $env:APPVEYOR_REPO_BRANCH"
+
+#---------------------------------#
+# BuildScript                     #
+#---------------------------------#
+Write-Host 'Nothing to build, skipping.....'
