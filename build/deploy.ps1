e2ea51b5d901e07ac8e86500e760472f17cc9385
Pete Maan <pete.maan+github@gmail.com>
2018-02-11 22:59:28 +0000

    UPDATE deploy.ps1
    Testing deploy of updated manifest back to github...

--- build/deploy.ps1
+++ build/deploy.ps1
@@ -15,9 +15,6 @@ Write-Host "`tBuild Version: `t$env:APPVEYOR_BUILD_VERSION"
 Write-Host "`tNew Version: `t$((Import-PowerShellDataFile $ManifestPath).ModuleVersion)"
 Write-Host "PR: $ENV:APPVEYOR_PULL_REQUEST_NUMBER"
 Write-Host "Branch: $ENV:APPVEYOR_REPO_BRANCH"
-#$ModuleManifestPath = Join-Path -path "$pwd" -ChildPath ("$env:ModuleName" + '.psd1')
-#$ModuleManifest = Get-Content $ModuleManifestPath -Raw
-#[regex]::replace($ModuleManifest, '(ModuleVersion = )(.*)', "`$1'$env:APPVEYOR_BUILD_VERSION'") | Out-File -LiteralPath $ModuleManifestPath
 
 #---------------------------------#
 # Publish to PS Gallery           #
@@ -34,3 +31,25 @@ Write-Host "Branch: $ENV:APPVEYOR_REPO_BRANCH"
 #Write-Host 'Publishing module to Powershell Gallery'
 #Uncomment the below line, make sure you set the variables in appveyor.yml
 #Publish-Module -Name $env:ModuleName -NuGetApiKey $env:NuGetApiKey
+
+Write-Host "Version Update in GitHub" -ForegroundColor Yellow
+
+git config --global core.safecrlf false
+
+git config --global credential.helper store
+
+Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:access_token):x-oauth-basic@github.com`n"
+
+git config --global user.email "pete.maan+github@gmail.com"
+
+git config --global user.name "Pete Maan"
+
+git checkout issue-44
+
+git add $(Join-Path "$env:APPVEYOR_PROJECT_NAME" "$env:APPVEYOR_PROJECT_NAME.psd1")
+
+git status
+
+git commit -s -m "Update Version"
+
+git push origin issue-44
