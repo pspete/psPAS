f8e73f00f263df3a4d8e06959be4b35a63967591
Pete Maan <pete.maan+github@gmail.com>
2018-02-12 13:14:13 +0000

    UPDATE deploy.ps1
    Removed update of version in manifest file.
    Added logic to "deploy" only if correct branch and pull request number
    not present.

--- build/deploy.ps1
+++ build/deploy.ps1
@@ -1,57 +1,53 @@
 #---------------------------------#
 # Header                          #
 #---------------------------------#
-Write-Host "Deploying" -ForegroundColor Yellow
+Write-Host "Deploy Process:" -ForegroundColor Yellow
 
-#---------------------------------#
-# Update module manifest          #
-#---------------------------------#
-Write-Host "Update Module Version"
-$ManifestPath = Join-Path "$pwd" $(Join-Path "$env:APPVEYOR_PROJECT_NAME" "$env:APPVEYOR_PROJECT_NAME.psd1")
-$CurrentVersion = (Import-PowerShellDataFile $ManifestPath).ModuleVersion
-Write-Host "`tCurrent Version: `t$CurrentVersion"
-Write-Host "`tBuild Version: `t$env:APPVEYOR_BUILD_VERSION"
-((Get-Content $ManifestPath).replace("= '$($currentVersion)'", "= '$($env:APPVEYOR_BUILD_VERSION)'")) | Set-Content $ManifestPath
-Write-Host "`tNew Version: `t$((Import-PowerShellDataFile $ManifestPath).ModuleVersion)"
-Write-Host "PR: $ENV:APPVEYOR_PULL_REQUEST_NUMBER"
-Write-Host "Branch: $ENV:APPVEYOR_REPO_BRANCH"
 
-#---------------------------------#
-# Publish to PS Gallery           #
-#---------------------------------#
-#if ($env:APPVEYOR_REPO_BRANCH -notmatch 'master') {
-#	Write-Host "Finished testing of branch: $env:APPVEYOR_REPO_BRANCH - Exiting"
-#	exit;
-#}
+if ((! $ENV:APPVEYOR_PULL_REQUEST_NUMBER) -and ($ENV:APPVEYOR_REPO_BRANCH -eq 'issue-44')) {
 
-#$ModulePath = Split-Path $pwd
-#Write-Host "Adding $ModulePath to 'psmodulepath' PATH variable"
-#$env:psmodulepath = $env:psmodulepath + ';' + $ModulePath
+	Write-Host "Not a PR & is Branch to publish"
+	#---------------------------------#
+	# Publish to PS Gallery           #
+	#---------------------------------#
+	#if ($env:APPVEYOR_REPO_BRANCH -notmatch 'master') {
+	#	Write-Host "Finished testing of branch: $env:APPVEYOR_REPO_BRANCH - Exiting"
+	#	exit;
+	#}
 
-#Write-Host 'Publishing module to Powershell Gallery'
-#Uncomment the below line, make sure you set the variables in appveyor.yml
-#Publish-Module -Name $env:ModuleName -NuGetApiKey $env:NuGetApiKey
+	#$ModulePath = Split-Path $pwd
+	#Write-Host "Adding $ModulePath to 'psmodulepath' PATH variable"
+	#$env:psmodulepath = $env:psmodulepath + ';' + $ModulePath
 
-<#
-Write-Host "Update Version on GitHub" -ForegroundColor Yellow
+	#Write-Host 'Publishing module to Powershell Gallery'
+	#Uncomment the below line, make sure you set the variables in appveyor.yml
+	#Publish-Module -Name $env:ModuleName -NuGetApiKey $env:NuGetApiKey
 
-git config --global core.safecrlf false
+	#---------------------------------#
+	# Publish to Master Branch        #
+	#---------------------------------#
+	<#
+	Write-Host "Update Version on GitHub" -ForegroundColor Yellow
 
-git config --global credential.helper store
+	git config --global core.safecrlf false
 
-Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:access_token):x-oauth-basic@github.com`n"
+	git config --global credential.helper store
 
-git config --global user.email "pete.maan+github@gmail.com"
+	Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:access_token):x-oauth-basic@github.com`n"
 
-git config --global user.name "Pete Maan"
+	git config --global user.email "pete.maan+github@gmail.com"
 
-git checkout -q issue-44
+	git config --global user.name "Pete Maan"
 
-git add $(Join-Path "$env:APPVEYOR_PROJECT_NAME" "$env:APPVEYOR_PROJECT_NAME.psd1")
+	git checkout -q issue-44
 
-git status
+	git add $(Join-Path "$env:APPVEYOR_PROJECT_NAME" "$env:APPVEYOR_PROJECT_NAME.psd1")
 
-git commit -s -m "Update Version"
+	git status
 
-git push --porcelain origin issue-44
-#>
+	git commit -s -m "Update Version"
+
+	git push --porcelain origin issue-44
+	#>
+
+}
