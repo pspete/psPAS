ea3a8364fe2f8d7f0811193325b0388d9fbd91f3
Pete Maan <pete.maan+github@gmail.com>
2018-02-12 14:48:43 +0000

    UPDATE deploy.ps1
    Added logic

--- build/deploy.ps1
+++ build/deploy.ps1
@@ -4,16 +4,11 @@
 Write-Host "Deploy Process:" -ForegroundColor Yellow
 
 
-if ((! $ENV:APPVEYOR_PULL_REQUEST_NUMBER) -and ($ENV:APPVEYOR_REPO_BRANCH -eq 'issue-44')) {
+if ((! $ENV:APPVEYOR_PULL_REQUEST_NUMBER) -and ($ENV:APPVEYOR_REPO_BRANCH -eq 'master')) {
 
-	Write-Host "Not a PR & is Branch to publish"
 	#---------------------------------#
 	# Publish to PS Gallery           #
 	#---------------------------------#
-	#if ($env:APPVEYOR_REPO_BRANCH -notmatch 'master') {
-	#	Write-Host "Finished testing of branch: $env:APPVEYOR_REPO_BRANCH - Exiting"
-	#	exit;
-	#}
 
 	#$ModulePath = Split-Path $pwd
 	#Write-Host "Adding $ModulePath to 'psmodulepath' PATH variable"
@@ -27,27 +22,45 @@ if ((! $ENV:APPVEYOR_PULL_REQUEST_NUMBER) -and ($ENV:APPVEYOR_REPO_BRANCH -eq 'i
 	# Publish to Master Branch        #
 	#---------------------------------#
 	<#
-	Write-Host "Update Version on GitHub" -ForegroundColor Yellow
+	Try{
 
-	git config --global core.safecrlf false
+		Write-Host "Update Version on GitHub" -ForegroundColor Yellow
 
-	git config --global credential.helper store
+		git config --global core.safecrlf false
 
-	Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:access_token):x-oauth-basic@github.com`n"
+		git config --global credential.helper store
 
-	git config --global user.email "pete.maan+github@gmail.com"
+		Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:access_token):x-oauth-basic@github.com`n"
 
-	git config --global user.name "Pete Maan"
+		git config --global user.email "pete.maan+github@gmail.com"
 
-	git checkout -q issue-44
+		git config --global user.name "Pete Maan"
 
-	git add $(Join-Path "$env:APPVEYOR_PROJECT_NAME" "$env:APPVEYOR_PROJECT_NAME.psd1")
+		git checkout -q issue-44
 
-	git status
+		git add $(Join-Path "$env:APPVEYOR_PROJECT_NAME" "$env:APPVEYOR_PROJECT_NAME.psd1")
 
-	git commit -s -m "Update Version"
+		git status
 
-	git push --porcelain origin issue-44
+		git commit -s -m "Update Version"
+
+		git push --porcelain origin issue-44
+
+	}
+
+	Catch{
+
+		Write-Warning "Publishing update to GitHub failed."
+		throw $_
+
+	}
 	#>
+
+}
+
+Else {
+
+	Write-Host "Finished testing of branch: $env:APPVEYOR_REPO_BRANCH - Exiting"
+	exit;
 
 }
