071d10f7f9c5a373c26619708d9c685fa3568043
Pete Maan <pete.maan+github@gmail.com>
2018-03-01 23:26:15 +0000

    ADD Pester Tests
    Added pester tests for "account" functions

---
+++ Tests/Get-PASAccount.Tests.ps1
@@ -0,0 +1,213 @@
+#Get Current Directory
+$Here = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+#Get Function Name
+$FunctionName = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -Replace ".Tests.ps1"
+
+#Assume ModuleName from Repo Root folder
+$ModuleName = Split-Path (Split-Path $Here -Parent) -Leaf
+
+#Resolve Path to Module Directory
+$ModulePath = Resolve-Path "$Here\..\$ModuleName"
+
+#Define Path to Module Manifest
+$ManifestPath = Join-Path "$ModulePath" "$ModuleName.psd1"
+
+if( -not (Get-Module -Name $ModuleName -All)) {
+
+	Import-Module -Name "$ManifestPath" -Force -ErrorAction Stop
+
+}
+
+BeforeAll {
+
+	$Script:RequestBody = $null
+
+}
+
+AfterAll {
+
+	$Script:RequestBody = $null
+
+}
+
+Describe $FunctionName {
+
+	InModuleScope $ModuleName {
+
+		Mock Invoke-PASRestMethod -MockWith {
+
+			$result = [pscustomobject]@{
+				"Count"    = 30
+				"Accounts" = [pscustomobject]@{
+					"AccountID"          = "66_6"
+					"Properties"         = @(
+						[pscustomobject]@{
+							"Key"   = "Safe"
+							"Value" = "zzTestSafe1"
+						},
+						[pscustomobject]@{
+							"Key"   = "Folder"
+							"Value" = "Root"
+						},
+						[pscustomobject]@{
+							"Key"   = "Name"
+							"Value" = "Operating System-_Test_WinDomain-VIRTUALREAL.IT-user01"
+						},
+						[pscustomobject]@{
+							"Key"   = "UserName"
+							"Value" = "user01"
+						},
+						[pscustomobject]@{
+							"Key"   = "PolicyID"
+							"Value" = "_Test_WinDomain"
+						},
+						[pscustomobject]@{
+							"Key"   = "LogonDomain"
+							"Value" = "VIRTUALREAL"
+						},
+						[pscustomobject]@{
+							"Key"   = "LastSuccessVerification"
+							"Value" = "1511973510"
+						},
+						[pscustomobject]@{
+							"Key"   = "Address"
+							"Value" = "VIRTUALREAL.IT"
+						},
+						[pscustomobject]@{
+							"Key"   = "DeviceType"
+							"Value" = "Operating System"
+						}
+					)
+					"InternalProperties" = @(
+						[pscustomobject]@{
+							"Key"   = "CPMStatus"
+							"Value" = "success"
+						},
+						[pscustomobject]@{
+							"Key"   = "SequenceID"
+							"Value" = "1"
+						},
+						[pscustomobject]@{
+							"Key"   = "CreationMethod"
+							"Value" = "PVWA"
+						},
+						[pscustomobject]@{
+							"Key"   = "RetriesCount"
+							"Value" = "-1"
+						},
+						[pscustomobject]@{
+							"Key"   = "LastSuccessChange"
+							"Value" = "1516127648"
+						},
+						[pscustomobject]@{
+							"Key"   = "LastTask"
+							"Value" = "ChangeTask"
+						}
+					)
+				}
+			}
+			return $result
+
+		}
+
+		$InputObj = [pscustomobject]@{
+			"sessionToken" = @{"Authorization" = "P_AuthValue"}
+			"WebSession"   = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+			"BaseURI"      = "https://P_URI"
+			"PVWAAppName"  = "P_App"
+			"Keywords"     = "SomeValue"
+			"Safe"         = "SomeSafe"
+		}
+
+		Context "Mandatory Parameters" {
+			$Parameters = @{Parameter = 'BaseURI'},
+			@{Parameter = 'SessionToken'}
+
+			It "specifies parameter <Parameter> as mandatory" -TestCases $Parameters {
+				param($Parameter)
+
+				{((Get-Command $FunctionName).Parameters["$Parameter"].Attributes).Mandatory} |
+					Should Be $true
+
+			}
+
+		}
+
+		$response = $InputObj | Get-PASAccount -WarningVariable warning -WarningAction SilentlyContinue
+
+		Context "Request Input" {
+
+			It "sends request" {
+
+				Assert-MockCalled Invoke-PASRestMethod -Times 1 -Exactly -Scope Describe
+
+			}
+
+			It "sends request to expected endpoint" {
+
+				Assert-MockCalled Invoke-PASRestMethod -ParameterFilter {
+
+					$URI -eq "$($InputObj.BaseURI)/$($InputObj.PVWAAppName)/WebServices/PIMServices.svc/Accounts?Keywords=SomeValue&Safe=SomeSafe"
+
+				} -Times 1 -Exactly -Scope Describe
+
+			}
+
+			It "sends request using expected method" {
+
+				Assert-MockCalled Invoke-PASRestMethod -ParameterFilter {$Method -match 'GET' } -Times 1 -Exactly -Scope Describe
+
+			}
+
+			It "sends request with no body" {
+
+				Assert-MockCalled Invoke-PASRestMethod -ParameterFilter {$Body -eq $null} -Times 1 -Exactly -Scope Describe
+
+			}
+
+		}
+
+		Context "Response Output" {
+
+			it "provides output" {
+
+				$response | Should not be null
+
+			}
+
+			It "has output with expected number of properties" {
+
+				($response | Get-Member -MemberType NoteProperty).length | Should Be 14
+
+			}
+
+			it "outputs object with expected typename" {
+
+				$response | get-member | select-object -expandproperty typename -Unique | Should Be psPAS.CyberArk.Vault.Account
+
+			}
+
+			it "writes warning that more than 1 account returned from the search" {
+
+				$warning | Should be "30 matching accounts found. Only the first result will be returned"
+
+			}
+
+			$DefaultProps = @{Property = 'sessionToken'},
+			@{Property = 'WebSession'},
+			@{Property = 'BaseURI'},
+			@{Property = 'PVWAAppName'}
+
+			It "returns default property <Property> in response" -TestCases $DefaultProps {
+				param($Property)
+
+				$response.$Property | Should Not Be Null
+
+			}
+
+		}
+
+	}
+
+}
---
+++ Tests/Set-PASAccount.Tests.ps1
@@ -0,0 +1,176 @@
+#Get Current Directory
+$Here = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+#Get Function Name
+$FunctionName = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -Replace ".Tests.ps1"
+
+#Assume ModuleName from Repository Root folder
+$ModuleName = Split-Path (Split-Path $Here -Parent) -Leaf
+
+#Resolve Path to Module Directory
+$ModulePath = Resolve-Path "$Here\..\$ModuleName"
+
+#Define Path to Module Manifest
+$ManifestPath = Join-Path "$ModulePath" "$ModuleName.psd1"
+
+if( -not (Get-Module -Name $ModuleName -All)) {
+
+	Import-Module -Name "$ManifestPath" -Force -ErrorAction Stop
+
+}
+
+BeforeAll {
+
+	$Script:RequestBody = $null
+
+}
+
+AfterAll {
+
+	$Script:RequestBody = $null
+
+}
+
+Describe $FunctionName {
+
+	InModuleScope $ModuleName {
+
+		Mock Invoke-PASRestMethod -MockWith {
+			[pscustomobject]@{
+				"UpdateAccountResult" = [pscustomobject]@{
+					"Prop1" = "Value1"
+					"Prop2" = "Value2"
+					"Prop3" = "Value3"
+					"Prop4" = "Value4"
+				}
+			}
+		}
+
+		$InputObj = [pscustomobject]@{
+			"sessionToken" = @{"Authorization" = "P_AuthValue"}
+			"WebSession"   = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+			"BaseURI"      = "https://P_URI"
+			"PVWAAppName"  = "P_App"
+			"AccountID"    = "12_3"
+			"Folder"       = "Root"
+			"AccountName"  = "Name"
+			"Name"         = "SomeName"
+			"PolicyID"     = "SomePolicy"
+			"Safe"         = "SafeName"
+			"Random"       = "Rand"
+			"Random1"      = "RandomValue"
+
+		}
+
+		[void]$InputObj.PSObject.TypeNames.Insert(0, "psPAS.CyberArk.Vault.Account")
+
+		Context "Mandatory Parameters" {
+
+			$Parameters = @{Parameter = 'BaseURI'},
+			@{Parameter = 'SessionToken'},
+			@{Parameter = 'AccountID'},
+			@{Parameter = 'Folder'},
+			@{Parameter = 'AccountName'}
+
+			It "specifies parameter <Parameter> as mandatory" -TestCases $Parameters {
+
+				param($Parameter)
+
+				{((Get-Command $FunctionName).Parameters["$Parameter"].Attributes).Mandatory} |
+					Should Be $true
+
+			}
+
+		}
+
+		$response = $InputObj | Set-PASAccount -Properties @{"Prop1" = "Val1"; "Prop2" = "Val2"; "Prop3" = "Val3"}
+
+		Context "Input" {
+
+			It "sends request" {
+
+				Assert-MockCalled Invoke-PASRestMethod -Times 1 -Exactly -Scope Describe
+
+			}
+
+			It "sends request to expected endpoint" {
+
+				Assert-MockCalled Invoke-PASRestMethod -ParameterFilter {
+
+					$URI -eq "$($InputObj.BaseURI)/$($InputObj.PVWAAppName)/WebServices/PIMServices.svc/Accounts/12_3"
+
+				} -Times 1 -Exactly -Scope Describe
+
+			}
+
+			It "uses expected method" {
+
+				Assert-MockCalled Invoke-PASRestMethod -ParameterFilter {$Method -match 'PUT' } -Times 1 -Exactly -Scope Describe
+
+			}
+
+			It "sends request with expected body" {
+
+				Assert-MockCalled Invoke-PASRestMethod -ParameterFilter {
+
+					$Script:RequestBody = $Body | ConvertFrom-Json
+
+					($Script:RequestBody.Accounts) -ne $null
+
+				} -Times 1 -Exactly -Scope Describe
+
+			}
+
+			It "has a request body with expected number of properties" {
+
+				($Script:RequestBody.Accounts | Get-Member -MemberType NoteProperty).length | Should Be 4
+
+			}
+
+			It "has a request body with expected number of nested properties" {
+
+				($Script:RequestBody.Accounts.Properties).count | Should Be 5
+
+			}
+
+		}
+
+		Context "Output" {
+
+			it "provides output" {
+
+				$response | Should not BeNullOrEmpty
+
+			}
+
+			It "has output with expected number of properties" {
+
+				($response | Get-Member -MemberType NoteProperty).length | Should Be 9
+
+			}
+
+			it "outputs object with expected typename" {
+
+				$response | get-member | select-object -expandproperty typename -Unique | Should Be psPAS.CyberArk.Vault.Account
+
+			}
+
+			$DefaultProps = @{Property = 'sessionToken'},
+			@{Property = 'WebSession'},
+			@{Property = 'BaseURI'},
+			@{Property = 'PVWAAppName'},
+			@{Property = 'AccountID'}
+
+
+			It "returns default property <Property> in response" -TestCases $DefaultProps {
+				param($Property)
+
+				$response.$Property | Should Not Be Null
+
+			}
+
+		}
+
+	}
+
+}
