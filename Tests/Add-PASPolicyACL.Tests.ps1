1d25954988d2ca6d07c4823d1f68d2590c9f8e72
Pete Maan <pete.maan+github@gmail.com>
2018-03-06 22:31:14 +0000

    ADD PolicyACL Tests
    Added Pester Tests for Policy ACL Functions

---
+++ Tests/Add-PASPolicyACL.Tests.ps1
@@ -0,0 +1,154 @@
+#Get Current Directory
+$Here = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+#Get Function Name
+$FunctionName = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -Replace ".Tests.ps1"
+
+#Assume ModuleName from Repository Root folder
+$ModuleName = Split-Path (Split-Path $Here -Parent) -Leaf
+
+#Resolve Path to Module Directory
+$ModulePath = Resolve-Path "$Here\..\$ModuleName"
+
+#Define Path to Module Manifest
+$ManifestPath = Join-Path "$ModulePath" "$ModuleName.psd1"
+
+if( -not (Get-Module -Name $ModuleName -All)) {
+
+	Import-Module -Name "$ManifestPath" -Force -ErrorAction Stop
+
+}
+
+BeforeAll {
+
+	$Script:RequestBody = $null
+
+}
+
+AfterAll {
+
+	$Script:RequestBody = $null
+
+}
+
+Describe $FunctionName {
+
+	InModuleScope $ModuleName {
+
+		Mock Invoke-PASRestMethod -MockWith {
+			[pscustomobject]@{"AddPolicyPrivilegedCommandResult" = [pscustomobject]@{"some" = "thing"}}
+
+		}
+
+		$InputObj = [pscustomobject]@{
+			"sessionToken" = @{"Authorization" = "P_AuthValue"}
+			"WebSession"   = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+			"BaseURI"      = "https://P_URI"
+			"PVWAAppName"  = "P_App"
+			"PolicyID"     = "UNIXSSH"
+		}
+
+		Context "Mandatory Parameters" {
+
+			$Parameters = @{Parameter = 'BaseURI'},
+			@{Parameter = 'SessionToken'},
+			@{Parameter = 'Command'},
+			@{Parameter = 'CommandGroup'},
+			@{Parameter = 'PermissionType'},
+			@{Parameter = 'PolicyID'},
+			@{Parameter = 'UserName'}
+
+
+			It "specifies parameter <Parameter> as mandatory" -TestCases $Parameters {
+
+				param($Parameter)
+
+				(Get-Command Add-PASPolicyACL).Parameters["$Parameter"].Attributes.Mandatory | Should Be $true
+
+			}
+
+		}
+
+		$response = $InputObj | Add-PASPolicyACL -UserName "root" -Command 'some command' -CommandGroup $false -PermissionType Deny
+
+		Context "Input" {
+
+			It "sends request" {
+
+				Assert-MockCalled Invoke-PASRestMethod -Times 1 -Exactly -Scope Describe
+
+			}
+
+			It "sends request to expected endpoint" {
+
+				Assert-MockCalled Invoke-PASRestMethod -ParameterFilter {
+
+					$URI -eq "$($InputObj.BaseURI)/$($InputObj.PVWAAppName)/WebServices/PIMServices.svc/Policy/UNIXSSH/PrivilegedCommands"
+
+				} -Times 1 -Exactly -Scope Describe
+
+			}
+
+			It "uses expected method" {
+
+				Assert-MockCalled Invoke-PASRestMethod -ParameterFilter {$Method -match 'PUT' } -Times 1 -Exactly -Scope Describe
+
+			}
+
+			It "sends request with expected body" {
+
+				Assert-MockCalled Invoke-PASRestMethod -ParameterFilter {
+
+					$Script:RequestBody = $Body | ConvertFrom-Json
+
+					($Script:RequestBody) -ne $null
+
+				} -Times 1 -Exactly -Scope Describe
+
+			}
+
+			It "has a request body with expected number of properties" {
+
+				($Script:RequestBody | Get-Member -MemberType NoteProperty).length | Should Be 4
+
+			}
+
+		}
+
+		Context "Output" {
+
+			it "provides output" {
+
+				$response | Should not BeNullOrEmpty
+
+			}
+
+			It "has output with expected number of properties" {
+
+				($response | Get-Member -MemberType NoteProperty).length | Should Be 5
+
+			}
+
+			it "outputs object with expected typename" {
+
+				$response | get-member | select-object -expandproperty typename -Unique | Should Be psPAS.CyberArk.Vault.ACL.Policy
+
+			}
+
+			$DefaultProps = @{Property = 'sessionToken'},
+			@{Property = 'WebSession'},
+			@{Property = 'BaseURI'},
+			@{Property = 'PVWAAppName'}
+
+			It "returns default property <Property> in response" -TestCases $DefaultProps {
+				param($Property)
+
+				$response.$Property | Should Not BeNullOrEmpty
+
+			}
+
+		}
+
+	}
+
+}
