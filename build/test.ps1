6a769dc1d93045c9a6835a9465b6b1783827e326
Pete Maan <pete.maan+github@gmail.com>
2018-03-08 22:33:18 +0000

    ADD Coveralls Support
    Added coveralls to modules installed from PowerShellGallery during
    build.
    Code Coverage information from Pester is uploaded to Coveralls.

--- build/test.ps1
+++ build/test.ps1
@@ -12,6 +12,11 @@ $res = Invoke-Pester -Path ".\Tests" -OutputFormat NUnitXml -OutputFile TestsRes
 Write-Host 'Uploading Test Results'
 (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\TestsResults.xml))
 
+Write-Host 'Formating Code Coverage'
+$coverage = Format-Coverage -RootFolder ".\psPAS" -PesterResults $res -CoverallsApiToken $($env:coveralls_key) -BranchName $($env:APPVEYOR_REPO_BRANCH)
+Write-Host 'Publishing Code Coverage'
+Publish-Coverage -Coverage $coverage
+
 #---------------------------------#
 # Validate                        #
 #---------------------------------#
