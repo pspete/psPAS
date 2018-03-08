9359b3c7eb8334a5d48540d13374d0c2c31dfd01
Pete Maan <pete.maan+github@gmail.com>
2018-03-08 23:52:45 +0000

    UPDATE New-PASAccountGroup.Tests
    Updated test input parameter to match change in Function (GroupPlatform
    changed to GroupPlatformID)

--- Tests/New-PASAccountGroup.Tests.ps1
+++ Tests/New-PASAccountGroup.Tests.ps1
@@ -40,13 +40,13 @@ Describe $FunctionName {
 		}
 
 		$InputObj = [pscustomobject]@{
-			"sessionToken"  = @{"Authorization" = "P_AuthValue"}
-			"WebSession"    = New-Object Microsoft.PowerShell.Commands.WebRequestSession
-			"BaseURI"       = "https://P_URI"
-			"PVWAAppName"   = "P_App"
-			"GroupName"     = "SomeName"
-			"GroupPlatform" = "SomePlatform"
-			"Safe"          = "SomeSafe"
+			"sessionToken"    = @{"Authorization" = "P_AuthValue"}
+			"WebSession"      = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+			"BaseURI"         = "https://P_URI"
+			"PVWAAppName"     = "P_App"
+			"GroupName"       = "SomeName"
+			"GroupPlatformID" = "SomePlatform"
+			"Safe"            = "SomeSafe"
 		}
 
 		Context "Mandatory Parameters" {
