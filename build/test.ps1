2ca3b977d8bfc63b89077313cdbd209ce449f507
Pete Maan <pete.maan+github@gmail.com>
2018-03-09 11:16:33 +0000

    UPDATE test.ps1
    Removed specification of RootFolder parameter on call to Format-Coverage
    for coveralls.io

--- build/test.ps1
+++ build/test.ps1
@@ -15,7 +15,7 @@ Write-Host 'Uploading Test Results'
 (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\TestsResults.xml))
 
 Write-Host 'Formating Code Coverage'
-$coverage = Format-Coverage -RootFolder ".\psPAS" -PesterResults $res -CoverallsApiToken $($env:coveralls_key) -BranchName $($env:APPVEYOR_REPO_BRANCH)
+$coverage = Format-Coverage -PesterResults $res -CoverallsApiToken $($env:coveralls_key) -BranchName $($env:APPVEYOR_REPO_BRANCH)
 Write-Host 'Publishing Code Coverage'
 Publish-Coverage -Coverage $coverage
 
