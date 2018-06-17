b971064be038399b6ae2d9d834c174ee07f3aff1
Pete Maan <pete.maan+github@gmail.com>
2018-06-17 14:02:40 +0100

    UPDATE psPAS.psm1
    Changed dot sourcing code to improve speed of module import and reporting of errors during import.

--- psPAS/psPAS.psm1
+++ psPAS/psPAS.psm1
@@ -1,39 +1,37 @@
 ﻿<#
 .SYNOPSIS
+Reads all required module files
 
 .DESCRIPTION
+Dot Sources each ps1 file under a module directory
 
 .EXAMPLE
-
-.INPUTS
-
-.OUTPUTS
-
-.NOTES
-
-.LINK
+Import-Module ModuleFile.psm1
 
 #>
-[CmdletBinding()]  
+[CmdletBinding()]
 param()
 
 #Get function files
-Get-ChildItem $PSScriptRoot\ -Recurse -Filter "*.ps1" -Exclude "*.ps1xml" | 
+Get-ChildItem $PSScriptRoot\ -Recurse -Include "*.ps1" -Exclude "*.ps1xml" |
 
-    ForEach-Object {
+ForEach-Object {
 
-        Try{
-            
-            #Dot Source each file
-            . $_.fullname
+	Try {
 
-        }
+		$ErrorActionPreference = "Stop"
+		$FunctionFile = $_.fullname
+		# Dot Source each file
+		. (
+			[scriptblock]::Create(
+				[io.file]::ReadAllText($FunctionFile, [Text.Encoding]::UTF8)
+			)
+		)
 
-        Catch{
+	} Catch {
 
-            Write-Error "Failed to import function $($_.fullname)"
+		throw "Failed to import function $FunctionFile"
 
-        }
-        
-    
-    }
+	}
+
+}
