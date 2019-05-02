555fd63b159a9046e80f7882d6dbb08e4053f715
Pete Maan <pete.maan+github@gmail.com>
2019-05-02 21:31:28 +0100

    UPDATE New-PASUser
    Added support for new api methods introduced in 10.9

--- Tests/New-PASUser.Tests.ps1
+++ Tests/New-PASUser.Tests.ps1
@@ -13,7 +13,7 @@ $ModulePath = Resolve-Path "$Here\..\$ModuleName"
 #Define Path to Module Manifest
 $ManifestPath = Join-Path "$ModulePath" "$ModuleName.psd1"
 
-if( -not (Get-Module -Name $ModuleName -All)) {
+if ( -not (Get-Module -Name $ModuleName -All)) {
 
 	Import-Module -Name "$ManifestPath" -ArgumentList $true -Force -ErrorAction Stop
 
@@ -35,29 +35,10 @@ Describe $FunctionName {
 
 	InModuleScope $ModuleName {
 
-		Mock Invoke-PASRestMethod -MockWith {
-			[PSCustomObject]@{"Detail1" = "Detail"; "Detail2" = "Detail"}
-		}
-
-		$InputObj = [pscustomobject]@{
-			"sessionToken"    = @{"Authorization" = "P_AuthValue"}
-			"WebSession"      = New-Object Microsoft.PowerShell.Commands.WebRequestSession
-			"BaseURI"         = "https://P_URI"
-			"PVWAAppName"     = "P_App"
-			"UserName"        = "SomeUser"
-			"InitialPassword" = $("P_Password" | ConvertTo-SecureString -AsPlainText -Force)
-			"FirstName"       = "Some"
-			"LastName"        = "User"
-			"ExpiryDate"      = "10/31/2018"
-
-		}
-
 		Context "Mandatory Parameters" {
 
-			$Parameters = @{Parameter = 'BaseURI'},
-			@{Parameter = 'SessionToken'},
-			@{Parameter = 'UserName'},
-			@{Parameter = 'InitialPassword'}
+			$Parameters = @{Parameter = 'BaseURI' },
+			@{Parameter = 'SessionToken' }
 
 			It "specifies parameter <Parameter> as mandatory" -TestCases $Parameters {
 
@@ -67,15 +48,54 @@ Describe $FunctionName {
 
 			}
 
+			It "specifies parameter UserName as mandatory for ParameterSet legacy" {
+
+				(Get-Command New-PASUser).Parameters["UserName"].ParameterSets["legacy"].IsMandatory | Should be $true
+
+			}
+
+			It "specifies parameter UserName as mandatory for ParameterSet 10_9" {
+
+				(Get-Command New-PASUser).Parameters["UserName"].ParameterSets["10_9"].IsMandatory | Should be $true
+
+			}
+
+			It "specifies parameter InitialPassword as mandatory for ParameterSet legacy" {
+
+				(Get-Command New-PASUser).Parameters["InitialPassword"].ParameterSets["legacy"].IsMandatory | Should be $true
+
+			}
+
 		}
 
-		$response = $InputObj | New-PASUser
+		Context "Input - legacy" {
 
-		Context "Input" {
+			BeforeEach {
+
+				Mock Invoke-PASRestMethod -MockWith {
+					[PSCustomObject]@{"Detail1" = "Detail"; "Detail2" = "Detail" }
+				}
+
+				$InputObj = [pscustomobject]@{
+					"sessionToken"    = @{"Authorization" = "P_AuthValue" }
+					"WebSession"      = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+					"BaseURI"         = "https://P_URI"
+					"PVWAAppName"     = "P_App"
+					"UserName"        = "SomeUser"
+					"InitialPassword" = $("P_Password" | ConvertTo-SecureString -AsPlainText -Force)
+					"FirstName"       = "Some"
+					"LastName"        = "User"
+					"ExpiryDate"      = "10/31/2018"
+
+				}
+
+				$response = $InputObj | New-PASUser -UseV9API
+
+			}
 
 			It "sends request" {
 
-				Assert-MockCalled Invoke-PASRestMethod -Times 1 -Exactly -Scope Describe
+				Assert-MockCalled Invoke-PASRestMethod -Times 1 -Exactly -Scope It
 
 			}
 
@@ -85,13 +105,13 @@ Describe $FunctionName {
 
 					$URI -eq "$($InputObj.BaseURI)/$($InputObj.PVWAAppName)/WebServices/PIMServices.svc/Users"
 
-				} -Times 1 -Exactly -Scope Describe
+				} -Times 1 -Exactly -Scope It
 
 			}
 
 			It "uses expected method" {
 
-				Assert-MockCalled Invoke-PASRestMethod -ParameterFilter {$Method -match 'POST' } -Times 1 -Exactly -Scope Describe
+				Assert-MockCalled Invoke-PASRestMethod -ParameterFilter { $Method -match 'POST' } -Times 1 -Exactly -Scope It
 
 			}
 
@@ -103,7 +123,7 @@ Describe $FunctionName {
 
 					($Script:RequestBody) -ne $null
 
-				} -Times 1 -Exactly -Scope Describe
+				} -Times 1 -Exactly -Scope It
 
 			}
 
@@ -115,7 +135,107 @@ Describe $FunctionName {
 
 		}
 
+		Context "Input - V10" {
+
+			BeforeEach {
+
+				Mock Invoke-PASRestMethod -MockWith {
+					[PSCustomObject]@{"Detail1" = "Detail"; "Detail2" = "Detail" }
+				}
+
+				$InputObj = [pscustomobject]@{
+					"sessionToken"    = @{"Authorization" = "P_AuthValue" }
+					"WebSession"      = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+					"BaseURI"         = "https://P_URI"
+					"PVWAAppName"     = "P_App"
+					"UserName"        = "SomeUser"
+					"InitialPassword" = $("P_Password" | ConvertTo-SecureString -AsPlainText -Force)
+					"FirstName"       = "Some"
+					"LastName"        = "User"
+					"ExpiryDate"      = "10/31/2018"
+					"workStreet"      = "SomeStreet"
+					"homePage"        = "www.geocities.com"
+					"faxNumber"       = "1979"
+
+				}
+
+				$response = $InputObj | New-PASUser
+
+			}
+
+			It "sends request" {
+
+				Assert-MockCalled Invoke-PASRestMethod -Times 1 -Exactly -Scope It
+
+			}
+
+			It "sends request to expected endpoint" {
+
+				Assert-MockCalled Invoke-PASRestMethod -ParameterFilter {
+
+					$URI -eq "$($InputObj.BaseURI)/$($InputObj.PVWAAppName)/api/Users"
+
+				} -Times 1 -Exactly -Scope It
+
+			}
+
+			It "uses expected method" {
+
+				Assert-MockCalled Invoke-PASRestMethod -ParameterFilter { $Method -match 'POST' } -Times 1 -Exactly -Scope It
+
+			}
+
+			It "sends request with expected body" {
+
+				Assert-MockCalled Invoke-PASRestMethod -ParameterFilter {
+
+					$Script:RequestBody = $Body | ConvertFrom-Json
+
+					($Script:RequestBody) -ne $null
+
+				} -Times 1 -Exactly -Scope It
+
+			}
+
+			It "has a request body with expected number of properties" {
+
+				($Script:RequestBody | Get-Member -MemberType NoteProperty).length | Should Be 7
+
+			}
+
+			It "throws error if version requirement not met" {
+
+				{ $InputObj | New-PASUser -ExternalVersion "1.0" } | Should Throw
+
+			}
+
+
+		}
+
 		Context "Output" {
+
+			BeforeEach {
+
+				Mock Invoke-PASRestMethod -MockWith {
+					[PSCustomObject]@{"Detail1" = "Detail"; "Detail2" = "Detail" }
+				}
+
+				$InputObj = [pscustomobject]@{
+					"sessionToken"    = @{"Authorization" = "P_AuthValue" }
+					"WebSession"      = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+					"BaseURI"         = "https://P_URI"
+					"PVWAAppName"     = "P_App"
+					"UserName"        = "SomeUser"
+					"InitialPassword" = $("P_Password" | ConvertTo-SecureString -AsPlainText -Force)
+					"FirstName"       = "Some"
+					"LastName"        = "User"
+					"ExpiryDate"      = "10/31/2018"
+
+				}
+
+				$response = $InputObj | New-PASUser -UseV9API
+
+			}
 
 			it "provides output" {
 
@@ -135,11 +255,17 @@ Describe $FunctionName {
 
 			}
 
-			$DefaultProps = @{Property = 'sessionToken'},
-			@{Property = 'WebSession'},
-			@{Property = 'BaseURI'},
-			@{Property = 'PVWAAppName'},
-			@{Property = 'ExternalVersion'}
+			it "outputs object with expected typename - V10" {
+				$response = $InputObj | New-PASUser
+				$response | get-member | select-object -expandproperty typename -Unique | Should Be psPAS.CyberArk.Vault.User.Extended
+
+			}
+
+			$DefaultProps = @{Property = 'sessionToken' },
+			@{Property = 'WebSession' },
+			@{Property = 'BaseURI' },
+			@{Property = 'PVWAAppName' },
+			@{Property = 'ExternalVersion' }
 
 			It "returns default property <Property> in response" -TestCases $DefaultProps {
 				param($Property)
