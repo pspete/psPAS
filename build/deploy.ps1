fa34e856b06b8e040467eb2be09ff25cfb928ad4
Pete Maan <pete.maan+github@gmail.com>
2019-12-13 02:23:24 +0000

    :construction_worker: UPDATE deploy.ps1
    Adding `-SkipAutomaticTags` to `Publish-Module` command.
    Fix hanging deployment due to too many tags.

--- build/deploy.ps1
+++ build/deploy.ps1
@@ -15,7 +15,7 @@ if ((! $ENV:APPVEYOR_PULL_REQUEST_NUMBER) -and ($ENV:APPVEYOR_REPO_BRANCH -eq 'm
 
 		$ModulePath = Join-Path $env:APPVEYOR_BUILD_FOLDER $env:APPVEYOR_PROJECT_NAME
 
-		Publish-Module -Path $ModulePath -NuGetApiKey $($env:psgallery_key) -Confirm:$false -ErrorAction Stop
+		Publish-Module -Path $ModulePath -NuGetApiKey $($env:psgallery_key) -SkipAutomaticTags -Confirm:$false -ErrorAction Stop
 
 		Write-Host "$($env:APPVEYOR_PROJECT_NAME) published." -ForegroundColor Cyan
 
