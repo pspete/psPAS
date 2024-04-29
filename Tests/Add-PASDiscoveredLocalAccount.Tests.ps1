c6c28e2067d769bf6f33369b6debd0ac32f235c9
Pete Maan <pete.maan+github@gmail.com>
2024-04-29 23:48:15 +0100

    Add Tests
    Adds initial Test files for new functions

---
+++ Tests/Add-PASDiscoveredLocalAccount.Tests.ps1
@@ -0,0 +1,98 @@
+Describe $($PSCommandPath -Replace '.Tests.ps1') {
+
+    BeforeAll {
+        #Get Current Directory
+        $Here = Split-Path -Parent $PSCommandPath
+
+        #Assume ModuleName from Repository Root folder
+        $ModuleName = Split-Path (Split-Path $Here -Parent) -Leaf
+
+        #Resolve Path to Module Directory
+        $ModulePath = Resolve-Path "$Here\..\$ModuleName"
+
+        #Define Path to Module Manifest
+        $ManifestPath = Join-Path "$ModulePath" "$ModuleName.psd1"
+
+        if ( -not (Get-Module -Name $ModuleName -All)) {
+
+            Import-Module -Name "$ManifestPath" -ArgumentList $true -Force -ErrorAction Stop
+
+        }
+
+        $Script:RequestBody = $null
+        $psPASSession = [ordered]@{
+            BaseURI            = 'https://SomeURL.cyberark.cloud/SomeApp'
+            ApiURI             = 'https://SomeURL.cyberark.cloud'
+            User               = $null
+            ExternalVersion    = [System.Version]'0.0'
+            WebSession         = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+            StartTime          = $null
+            ElapsedTime        = $null
+            LastCommand        = $null
+            LastCommandTime    = $null
+            LastCommandResults = $null
+        }
+
+        New-Variable -Name psPASSession -Value $psPASSession -Scope Script -Force
+
+    }
+
+
+    AfterAll {
+
+        $Script:RequestBody = $null
+
+    }
+
+    InModuleScope $(Split-Path (Split-Path (Split-Path -Parent $PSCommandPath) -Parent) -Leaf ) {
+
+        BeforeEach {
+            $psPASSession.ExternalVersion = '0.0'
+
+            #TODO: Figure out how to include Assert-VersionRequirement in P Cloud function tests
+            Mock Assert-VersionRequirement -MockWith {}
+
+            Mock Invoke-PASRestMethod -MockWith {
+                [PSCustomObject]@{'Prop1' = 'Val1'; 'Prop2' = 'Val2' }
+            }
+
+            $InputObject = [PSCustomObject]@{
+
+                'type'        = 'windows'
+                'identifiers' = @{'SomeProperty' = 'SomeValue' }
+
+            }
+
+            $response = $InputObject | Add-PASDiscoveredLocalAccount
+
+        }
+
+        Context 'Input' {
+
+            It 'sends request' {
+
+                Assert-MockCalled Invoke-PASRestMethod -Times 1 -Exactly -Scope It
+
+            }
+
+            It 'sends request to expected endpoint' {
+
+                Assert-MockCalled Invoke-PASRestMethod -ParameterFilter {
+
+                    $URI -eq "$($Script:psPASSession.ApiURI)/api/discovered-accounts/"
+
+                } -Times 1 -Exactly -Scope It
+
+            }
+
+            It 'uses expected method' {
+
+                Assert-MockCalled Invoke-PASRestMethod -ParameterFilter { $Method -match 'PUT' } -Times 1 -Exactly -Scope It
+
+            }
+
+        }
+
+    }
+
+}
