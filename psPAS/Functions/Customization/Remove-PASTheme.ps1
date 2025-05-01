29cb36c9a9055da2debde45f10e7079aff11070e
johannesconsulting <johannes@jp-consulting.se>
2025-05-01 20:28:02 +0200

    Added Customization APIs
    Adds Activate and Delete theme functions

---
+++ psPAS/Functions/Customization/Remove-PASTheme.ps1
@@ -0,0 +1,34 @@
+# .ExternalHelp psPAS-help.xml
+Function Remove-PASTheme {
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [parameter(
+            Mandatory = $true,
+            ValueFromPipelinebyPropertyName = $true
+        )]
+        [string]$ThemeName
+
+    )
+
+    BEGIN {
+
+        Assert-VersionRequirement -RequiredVersion 14.4
+
+    }#begin
+
+    PROCESS {
+
+        #Create URL for request
+        $URI = "$($psPASSession.BaseURI)/API/Themes/$($ThemeName | Get-EscapedString)"
+
+        if ($PSCmdlet.ShouldProcess($ThemeName, 'Removing UI Theme')) {
+
+			#send request to web service
+			Invoke-PASRestMethod -Uri $URI -Method DELETE
+
+		}
+
+    }#process
+
+    END { }#end
+}
