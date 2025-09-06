28b0ac8a88adb94991ffda36fdca4edcee08c5f0
johannesconsulting <johannes@jp-consulting.se>
2025-09-06 18:06:14 +0200

    Combined Get-PASPTASecurityConfigurationCategory so it can also list specifc info about each catogery based on key

--- psPAS/Functions/EventSecurity/Get-PASPTASecurityConfigurationCategory.ps1
+++ psPAS/Functions/EventSecurity/Get-PASPTASecurityConfigurationCategory.ps1
@@ -1,5 +1,24 @@
 # .ExternalHelp psPAS-help.xml
 Function Get-PASPTASecurityConfigurationCategory {
+    [CmdletBinding(SupportsShouldProcess = $false, DefaultParameterSetName = 'ListAllCategories')]
+    param(
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipelinebyPropertyName = $false,
+            ParameterSetName = 'ListAllCategories'
+        )]
+        [switch]$ListAllCategories,
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipelinebyPropertyName = $false,
+            ParameterSetName = 'categoryKey'
+        )]
+        [ValidateSet('ActiveDormantUser', 'PrivilegedUsersAndGroups', 'IrregularIpUser', 'SuspectedCredentialsTheft', 'InteractiveLogonWithServiceAccount',
+            'IrregularHoursUser', 'UnmanagedPrivilegedAccess', 'SuspiciousActivityInPSMSession', 'IrregularDaysUser', 'FailedVaultLogonAttempts',
+            'ExcessiveAccessUser', 'SuspiciousPasswordChange')]
+        [Alias('Category')]
+        [string]$categoryKey
+    )
 
     BEGIN {
         Assert-VersionRequirement -SelfHosted
@@ -8,22 +27,47 @@ Function Get-PASPTASecurityConfigurationCategory {
 
     PROCESS {
 
-        #Create request URL
-        $URI = "$($psPASSession.BaseURI)/API/pta/API/configuration/categories"
+        switch ($PSCmdlet.ParameterSetName) {
 
-        #send request to web service
-        $result = Invoke-PASRestMethod -Uri $URI -Method GET
+            'ListAllCategories' {
 
-        If ($null -ne $result) {
+                #Create request URL
+                $URI = "$($psPASSession.BaseURI)/API/pta/API/configuration/categories"
 
-            #Return Results as objects
-            $result | ForEach-Object {
-                [PSCustomObject]@{
-                    Category = $_
+                #send request to web service
+                $result = Invoke-PASRestMethod -Uri $URI -Method GET
+
+                If ($null -ne $result) {
+
+                    #Return Results as objects
+                    $result | ForEach-Object {
+                        [PSCustomObject]@{
+                            Category = $_
+                        }
+                    }
+
                 }
+
+            }
+
+            'categoryKey' {
+
+                #Create URL for Request
+                $URI = "$($psPASSession.BaseURI)/API/pta/API/configuration/categories/$categoryKey"
+
+                #send request to web service
+                $result = Invoke-PASRestMethod -Uri $URI -Method GET
+
+                If ($null -ne $result) {
+
+                    $result 
+
+                }
+
             }
 
         }
+
 
     }#process
 
